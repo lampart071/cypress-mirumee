06738d0f253c4a7dec85282847be26a36e9043be
lampart071 <lampart071@gmail.com>
2020-08-16 03:08:02 +0200

    added assertion to login api test

--- cypress/integration/saleor/loginapi.spec.js
+++ cypress/integration/saleor/loginapi.spec.js
@@ -3,6 +3,7 @@ context("Login Api", () => {
 
   it("navigates to succesful login", () => {
     cy.login();
-    cy.visit("https://demo.saleor.io");
+    cy.visit("https://demo.saleor.io")
+    cy.get("[data-testid='user-btn'] > li.main-menu__icon.main-menu__user--active");
   });
 });
